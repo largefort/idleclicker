0b387406728bcd087c8ff7d5d0f92e9396e40cf4
doomguyslays <122403136+doomguyslays@users.noreply.github.com>
2023-05-05 14:59:28 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
 
-let money = 0;
-let clickValue = 1;
-let autoClickers = 0;
-let autoClickerCost = 50;
-let quality = 1; // default quality level
-
-// Load saved game state from local storage
-function loadGame() {
-  const savedGame = JSON.parse(localStorage.getItem('idle-money-clicker-save'));
-  if (savedGame !== null) {
-    money = savedGame.money;
-    clickValue = savedGame.clickValue;
-    autoClickers =
