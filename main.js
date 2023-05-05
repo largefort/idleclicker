abfa4700168001bcb6a4b823833e985a135e2f0e
doomguyslays <122403136+doomguyslays@users.noreply.github.com>
2023-05-05 15:44:36 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let money = 0;
 let autoclickers = 0;
-let autoclickerCost = 10;
 let upgradeCost = 50;
 let upgrades = [
     {name: "Faster Clicking", cost: 100, effect: function() {
@@ -9,7 +8,7 @@ let upgrades = [
         }, 1000);
     }},
     {name: "Double Money", cost: 200, effect: function() {
-        document.getElementById("clickBtn").addEventListener("click", function() {
+        document.addEventListener("click", function() {
             click();
             click();
         });
@@ -17,23 +16,17 @@ let upgrades = [
 ];
 
 function click() {
-    money++;
+    money += autoclickers + 1;
     document.getElementById("money").innerHTML = money;
     document.getElementById("moneySound").play();
 }
 
-function buyAutoclicker() {
-    if (money >= autoclickerCost) {
-        money -= autoclickerCost;
-        autoclickers++;
-        document.getElementById("money").innerHTML = money;
-        document.getElementById("autoclickers").innerHTML = autoclickers;
-        autoclickerCost = Math.round(autoclickerCost * 1.1);
-        document.getElementById("autoclickerCost").innerHTML = autoclickerCost;
-        setInterval(function() {
-            click();
-        }, 1000);
-    }
+function getFreeAutoclicker() {
+    autoclickers++;
+    document.getElementById("autoclickers").innerHTML = autoclickers;
+    document.getElementById("buyAutoclickerBtn").innerHTML = "Click for Money";
+    document.getElementById("buyAutoclickerBtn").removeEventListener("click", getFreeAutoclicker);
+    document.getElementById("buyAutoclickerBtn").addEventListener("click", click);
 }
 
 function buyUpgrade() {
@@ -42,4 +35,20 @@ function buyUpgrade() {
         document.getElementById("money").innerHTML = money;
         upgradeCost = Math.round(upgradeCost * 1.5);
         document.getElementById("upgradeCost").innerHTML = upgradeCost;
-        let upgrade =
+        let upgrade = upgrades[Math.floor(Math.random() * upgrades.length)];
+        let li = document.createElement("li");
+        li.innerHTML = upgrade.name + " (costs " + upgrade.cost + " money)";
+        li.addEventListener("click", function() {
+            if (money >= upgrade.cost) {
+                money -= upgrade.cost;
+                document.getElementById("money").innerHTML = money;
+                upgrade.effect();
+                li.parentNode.removeChild(li);
+            }
+        });
+        document.getElementById("upgrades").appendChild(li);
+    }
+}
+
+document.getElementById("buyAutoclickerBtn").addEventListener("click", getFreeAutoclicker);
+document.getElementById("buyUpgradeBtn").addEventListener("click", buyUpgrade);
